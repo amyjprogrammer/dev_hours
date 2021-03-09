1677e555d04aac8b3d65debda34019c4c61c3aee
Amy Jones <amyjprogrammer@gmail.com>
2021-03-09 00:45:37 -0500

    New view and html

--- hours/views.py
+++ hours/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import HoursWorked
 from .forms import HoursWorkedForm
 
 """Home view"""
 def home(request):
-    context= {}
+    hours = HoursWorked.objects.all().order_by('-date_added')
+    context= {'hours': hours}
     return render(request, 'hours/home.html', context)
+
+def hours_worked(request):
+    """adding a form"""
+    if request.method != "POST":
+        #blank form
+        form = HoursWorkedForm()
+    else:
+        form = HoursWorkedForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('hours:home')
+
+    context = {'form': form}
+    return render('hours/hours_worked.html', context)
